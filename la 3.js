225e8e872064b58597a743d9de669c86a9a7afca
Carlosdmende <carlosdavidmendez.2007@gmail.com>
2026-06-05 13:58:46 -0400

    Add product details logging in la 3.js

---
+++ la 3.js
@@ -0,0 +1,9 @@
+const nombreproducto = "Tablet 345 pulgadas";
+let precio = 83484757588334.23243435
+let stock = 2345;
+const envioGratis = true;
+
+console.log(`nombre del producto: ${nombreproducto}`);
+console.log(`precio: ${precio}`);
+console.log(`cantidad en stock: ${stock}`);
+console.log(`recata tenei envio gratis: ${envioGratis}`);
