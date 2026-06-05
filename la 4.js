25491b4faa8537209a38d9b992e628aae54ce4d1
Carlosdmende <carlosdavidmendez.2007@gmail.com>
2026-06-05 13:59:33 -0400

    Create la 4.js

---
+++ la 4.js
@@ -0,0 +1,15 @@
+const colorsemaforo = "verde";
+
+if (colorsemaforo === "verde") {
+    console.log("Puede avanzar.");
+}
+else if (colorsemaforo === "amarillo") {
+    console.log("Reduzca la velocidad, precaución.");
+}
+
+else if (colorsemaforo === "rojo") {
+    console.log("Debe detenerse.");
+}
+else {
+    console.log("Color no válido.");
+}
